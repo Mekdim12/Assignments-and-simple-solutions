d79fdbb5c923fed25c348d4c0997d32b863a17e9
mekdim12 <jrmekdim50@gmail.com>
2024-03-20 11:23:52 +0300

    Add family tree scenario to the code

---
+++ family_tree.pl
@@ -0,0 +1,100 @@
+# current scenario: family tree
+
+"""
+    complexity of the family tree is 5>
+    there will be greate grandfather and greate grandmother
+    there will be grandfather and grandmother also would be greate unclue and aunt
+    there will be father and mother also would be uncle and aunt from each side of the family
+    there will be son and doughter and son's wife and doughter's husband and also cousine and cousine's wife and sibiling
+    there will be grand son and grand doughter and grand son's wife and grand doughter's husband and nephew and niece
+
+    image for this family tree scenario description: https://www.istockphoto.com/vector/family-tree-genealogy-diagram-pictogram-gm450540265-24669323
+    the whole idea is with prespective of husband write below father and mother
+"""
+
+# state the facts depening on the complexity of the family tree
+
+# defined for greate grandfather and grandmother
+male(abreham).
+female(yeshi).
+
+# defined for greate uncle and aunt
+male(alemu).
+female(alemenesh)
+
+# defined for grandfather and grandmother
+male(kebede).
+female(berkitu).
+
+# defined for father and mother
+male(yohanes).
+female(fkr).
+
+# defined for uncle and aunt 
+male(abebe).
+male(tamerat).
+male(wondwesen).
+female(kalkidan).
+female(mantegbosh).
+
+
+# defined for my self and wife
+male(mekdim).
+female(sari).
+
+# defined for brother and sister in law or brother's wife
+male(yeabsira).
+female(nuhamin).
+
+# defined for sister and brother in law or sister's husband
+male(buze).
+female(binni).
+
+# defined for cousine and cousine's wife
+male(jocy).
+female(meron).
+
+# daughter and son
+male(nati).
+female(nati).
+
+
+# define for nephew and niece from bother brother and sister
+# from brother side 
+male(girma).
+female(berut).
+
+# from thier sister side
+male(mikiyas).
+femalte(mesi).
+
+
+# define for grand cousin
+male(natanim).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
