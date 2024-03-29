1491645d66151e5384fd8a8b7e607af7d6bb908b
mekdim12 <jrmekdim50@gmail.com>
2024-03-29 18:13:47 +0300

    Add code for question number 3

---
+++ python-assginment/number3.py
@@ -0,0 +1,20 @@
+'''
+TODO: REMIND Bemnet i didn't run this to check the output
+
+For question number 3:
+    A: Output 
+        [0, 1, 2] 
+        ['Cse is the best', ;se is the greatest', 'ece is awesome']
+    B: Output
+        0
+        0
+        1
+        0
+        2
+    C: Output
+        a = Letter a
+        a = 3.14
+        a = 8
+        a = 7
+        a = Letter a
+'''
