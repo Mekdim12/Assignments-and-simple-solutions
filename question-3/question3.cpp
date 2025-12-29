bd3b7d5d54c9b30f89ebaf8a757a97e6967e3e45
Mekdim Tamirat <mekdimtamirat2014@gmail.com>
2025-12-30 01:07:40 +0300

    Add image and script files for question 3

--- question-3/question3.cpp
+++ question-3/question3.cpp
@@ -3,28 +3,27 @@
 
 using namespace std;
 
-// 1. Safe Addition Check
-void safe_add(int a, int b)
+void safe_add(int val_1, int val_2)
 {
     int max = numeric_limits<int>::max();
     int min = numeric_limits<int>::min();
 
-    // Logic: If a + b > max, then a > max - b
-    if (b > 0 && a > max - b)
+    //  If a + b > max, then a > max - b
+
+    if (val_2 > 0 && val_1 > max - val_2)
     {
-        cout << "[ADD] Overflow detected: " << a << " + " << b << " would exceed " << max << endl;
+        cout << "Overflow detected: " << val_1 << " + " << val_2 << " would exceed " << max << endl;
     }
-    else if (b < 0 && a < min - b)
+    else if (val_2 < 0 && val_1 < min - val_2)
     {
-        cout << "[ADD] Underflow detected: " << a << " + " << b << " would be less than " << min << endl;
+        cout << "Underflow detected: " << val_1 << " + " << val_2 << " would be less than " << min << endl;
     }
     else
     {
-        cout << "[ADD] Result: " << a + b << " (Safe)" << endl;
+        cout << "Result: " << val_1 + val_2 << " (Safe)" << endl;
     }
 }
 
-// 2. Safe Subtraction Check
 void safe_subtract(int a, int b)
 {
     int max = numeric_limits<int>::max();
@@ -32,19 +31,18 @@ void safe_subtract(int a, int b)
 
     if (b > 0 && a < min + b)
     {
-        cout << "[SUB] Underflow detected: " << a << " - " << b << " is below " << min << endl;
+        cout << "Underflow detected: " << a << " - " << b << " is below " << min << endl;
     }
     else if (b < 0 && a > max + b)
     {
-        cout << "[SUB] Overflow detected: " << a << " - " << b << " exceeds " << max << endl;
+        cout << "Overflow detected: " << a << " - " << b << " exceeds " << max << endl;
     }
     else
     {
-        cout << "[SUB] Result: " << a - b << " (Safe)" << endl;
+        cout << "Result: " << a - b << " (Safe)" << endl;
     }
 }
 
-// 3. Safe Multiplication Check
 void safe_multiply(int a, int b)
 {
     int max = numeric_limits<int>::max();
@@ -52,36 +50,36 @@ void safe_multiply(int a, int b)
 
     if (a > 0 && b > 0 && a > max / b)
     {
-        cout << "[MUL] Overflow detected!" << endl;
+        cout << "Overflow detected!" << endl;
     }
     else if (a > 0 && b < 0 && b < min / a)
     {
-        cout << "[MUL] Underflow detected!" << endl;
+        cout << "Underflow detected!" << endl;
     }
     else if (a < 0 && b > 0 && a < min / b)
     {
-        cout << "[MUL] Underflow detected!" << endl;
+        cout << "Underflow detected!" << endl;
     }
     else if (a < 0 && b < 0 && a < max / b)
     {
-        cout << "[MUL] Overflow detected!" << endl;
+        cout << "Overflow detected!" << endl;
     }
     else
     {
-        cout << "[MUL] Result: " << a * b << " (Safe)" << endl;
+        cout << "Result: " << a * b << " (Safe)" << endl;
     }
 }
 
 int main()
 {
-    int large_val = 2147483640;  // Near INT_MAX
-    int small_neg = -2147483640; // Near INT_MIN
+    int large_val = 2147483640;  // 2^31-1
+    int small_neg = -2147483640; // -2^31
 
     cout << "--- Testing Arithmetic Safety ---" << endl;
 
-    safe_add(large_val, 10);      // Should Overflow
-    safe_subtract(small_neg, 10); // Should Underflow
-    safe_multiply(1000000, 3000); // Should Overflow
+    safe_add(large_val, 1000);
+    safe_subtract(small_neg, 0);
+    safe_multiply(1000000, 3000);
 
     return 0;
 }
